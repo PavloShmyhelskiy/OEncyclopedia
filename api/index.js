1acf93649c9e342c5b4e68960b0996188588ce45
YevhenShvets <shvets_06@ukr.net>
2022-08-13 09:05:12 +0300

    fix: cors

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const userRoute = require("./routes/users");
 const articleRoute = require("./routes/articles");
 const groupRoute = require("./routes/groups");
 const tagRoute = require("./routes/tags");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -21,7 +22,14 @@ mongoose
     console.error(err);
   });
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
--- client/src/common/api/httpClient.ts
+++ client/src/common/api/httpClient.ts
@@ -4,6 +4,9 @@ import qs from 'qs'
 const httpClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   paramsSerializer: params => qs.stringify(params, { arrayFormat: 'comma' }),
+  headers: {
+    CORS: '*',
+  },
 })
 
 export default httpClient
